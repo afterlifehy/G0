65a574f5bd151792126cd67f4f74c88335c713b4
afterlifehy <757198090@qq.com>
2024-01-11 16:21:25 +0800

    打印告知书

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,13 +5,13 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 30001
-    const val versionName = "3.0.1"
+    const val versionCode = 20001
+    const val versionName = "1.8.6"
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
-    const val debug_is_dev = "true"
+    const val debug_is_dev = "false"
     const val debug_is_proxy = "true"
 
     const val release_is_debug = "false"
