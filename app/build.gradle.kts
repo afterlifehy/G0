a762d0619573b027b3b4ac0ce605b82ad410f148
afterlifehy <757198090@qq.com>
2024-06-05 16:04:24 +0800

    1.9.6 打包apk包名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,8 +103,13 @@ android {
     android.applicationVariants.all {
         outputs.all {
             if (this is com.android.build.gradle.internal.api.ApkVariantOutputImpl) {
-                this.outputFileName =
-                    "G0_${versionName}_${versionCode}_${buildType.name}_${if (buildType.name == "release") BuildConfig.release_is_dev else BuildConfig.debug_is_dev}.apk"
+                if (buildType.name == "release") {
+                    this.outputFileName =
+                        "G0_v${versionName}_${versionCode}_.apk"
+                } else {
+                    this.outputFileName =
+                        "G0_v${versionName}_${versionCode}_debug_${BuildConfig.release_is_dev}.apk"
+                }
             }
         }
     }
