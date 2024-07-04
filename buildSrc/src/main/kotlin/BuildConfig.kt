8431c6190d8db3aaeed558471730ba9685564657
afterlifehy <757198090@qq.com>
2024-07-04 16:52:42 +0800

    1.9.7_beta
    2.0.1_XH_alpha

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20025
-    const val versionName = "1.9.7_alpha"
+    const val versionCode = 20026
+    const val versionName = "1.9.7_beta"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
