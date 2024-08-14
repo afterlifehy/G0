2119359b2467707ce97bc80cf0fb9e268ce1be9e
afterlifehy <757198090@qq.com>
2024-08-14 16:29:25 +0800

    1.9.8_beta 新pos适配、主题色适配 2.0.1_XH_beta 1.9.8_YD_alpha

--- app/src/main/java/com/kernal/demo/plateid/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/kernal/demo/plateid/ui/activity/login/LoginActivity.kt
@@ -223,12 +223,14 @@ class LoginActivity : VbBaseActivity<LoginViewModel, ActivityLoginBinding>(), On
         jsonobject["longitude"] = lon.toString()
         jsonobject["latitude"] = lat.toString()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            jsonobject["imei"] = (getSystemService(TELEPHONY_SERVICE) as TelephonyManager).imei
-            val subscriptionManager = getSystemService(TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
-            val subscriptionInfoList = subscriptionManager.activeSubscriptionInfoList
-            if (subscriptionInfoList != null && !subscriptionInfoList.isEmpty()) {
-                jsonobject["simId"] = subscriptionInfoList[0].iccId
-            }
+//            jsonobject["imei"] = (getSystemService(TELEPHONY_SERVICE) as TelephonyManager).imei
+//            val subscriptionManager = getSystemService(TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
+//            val subscriptionInfoList = subscriptionManager.activeSubscriptionInfoList
+//            if (subscriptionInfoList != null && !subscriptionInfoList.isEmpty()) {
+//                jsonobject["simId"] = subscriptionInfoList[0].iccId
+//            }
+            jsonobject["imei"] = ""
+            jsonobject["simId"] = ""
         } else {
             jsonobject["imei"] = PhoneUtils.getIMEI()
             jsonobject["simId"] = (getSystemService(TELEPHONY_SERVICE) as TelephonyManager).simSerialNumber
--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20030
+    const val versionCode = 20031
     const val versionName = "1.9.8_beta"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
