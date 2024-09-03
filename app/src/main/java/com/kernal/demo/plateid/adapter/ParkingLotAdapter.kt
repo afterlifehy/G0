cc2e1cefcd151a59b74c0387acdb427a3e1f5ea9
afterlifehy <757198090@qq.com>
2024-09-03 16:21:12 +0800

    1.9.9_alpha 20032 第三张照片是否拍摄状态接口对接 2.0.1_XH 20027

--- app/src/main/java/com/kernal/demo/plateid/adapter/ParkingLotAdapter.kt
+++ app/src/main/java/com/kernal/demo/plateid/adapter/ParkingLotAdapter.kt
@@ -109,7 +109,7 @@ class ParkingLotAdapter(data: MutableList<ParkingLotBean>? = null, val onClickLi
                 if (System.currentTimeMillis() - item.orderStartTime > 60 * 60 * 1000) {
                     val num = (System.currentTimeMillis() - item.orderStartTime + 60 * 60 * 1000) / (30 * 60 * 1000)
                     val timeBegin = item.orderStartTime + 60 * 60 * 1000 + (30 * 60 * 1000) * num
-                    if (timeBegin > item.orderStartTime) {
+                    if (timeBegin > item.thirdPicTime) {
                         holder.vb.ivCamera.show()
                     } else {
                         holder.vb.ivCamera.gone()
--- base/src/main/java/com/kernal/demo/base/base/mvvm/UrlManager.kt
+++ base/src/main/java/com/kernal/demo/base/base/mvvm/UrlManager.kt
@@ -2,7 +2,7 @@ package com.kernal.demo.base.base.mvvm
 import com.kernal.demo.base.BuildConfig
 
 object UrlManager {
-//    const val DEV_HOST = "http://10.0.0.73:8080/ipms/service/"
+//    const val DEV_HOST = "http://192.168.0.39:8080/ipms/service/"
     const val DEV_HOST = "http://114.94.20.110/ipms/service/"
     const val FORMAL_HOST = "http://ipms.csnits.com/ipms/service/"
 
