aeee7d706a734fe242d0e0326cb690c553583db4
afterlifehy <757198090@qq.com>
2024-12-03 16:38:48 +0800

    1.9.10 20043 页面不显示时，不让操作弹框、 拍照软键盘隐藏、 ResponseBody 被消耗 2.0.3_beta 20055

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,15 +5,15 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20042
-    const val versionName = "1.9.10_beta2"
+    const val versionCode = 20043
+    const val versionName = "1.9.10"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
     const val debug_is_dev = "false"
     const val debug_is_proxy = "true"
 
-    const val release_is_debug = "true"
+    const val release_is_debug = "false"
     const val release_is_dev = "false"
     const val release_is_proxy = "false"
 }
