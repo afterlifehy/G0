01238b96d480a4a5e0147f797194fd0fcfeae7b9
afterlifehy <757198090@qq.com>
2025-04-24 14:26:48 +0800

    1.9.12 20047 对接取票开票二维码接口

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20045
-    const val versionName = "1.9.11"
+    const val versionCode = 20047
+    const val versionName = "1.9.12"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
