b02d340c2ad09869f7009bb16d9eb08226b12906
afterlifehy <757198090@qq.com>
2025-05-06 14:34:23 +0800

    1.9.13_beta 20200

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20050
+    const val versionCode = 20200
     const val versionName = "1.9.13_beta"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
