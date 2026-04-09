75d6101dd77c268df60265cbb7d62b9a81fb54c4
胡永 <16890793+afterlifehyy@user.noreply.gitee.com>
2026-04-09 15:02:35 +0800

    2.0.7_beta 20209 migration

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,8 +5,8 @@ object BuildConfig {
     const val minSdk = 23
     const val targetSdk = 33
 
-    const val versionCode = 20211
-    const val versionName = "2.0.7"
+    const val versionCode = 20212
+    const val versionName = "2.0.8_beta"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     const val debug_is_debug = "true"
